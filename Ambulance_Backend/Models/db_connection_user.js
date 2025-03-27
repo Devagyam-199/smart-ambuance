8db24886612c98614e0387f774eb35484f395e30
Devagyam-199 <160000074+Devagyam-199@users.noreply.github.com>
2025-03-28 00:01:54 +0530

    just fix

--- Ambulance_Backend/Models/db_connection_user.js
+++ Ambulance_Backend/Models/db_connection_user.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose')
-const mongo_url = process.env.MONGO_CONN
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
-mongoose.connect(mongo_url)
-    .then(()=>(
-        console.log(`connection sucessful to user db`)
-    )).catch((err)=>{
-        console.log(`connecion failed : ${err}`);
-        
-    })
+const mongo_url = process.env.MONGO_CONN;
+
+if (!mongo_url) {
+    console.error("MONGO_CONN is not defined. Check your environment variables.");
+    process.exit(1);
+}
+
+mongoose.connect(mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connection successful to MongoDB"))
+.catch((err) => {
+    console.error(`Connection failed: ${err.message}`);
+    process.exit(1);
+});
