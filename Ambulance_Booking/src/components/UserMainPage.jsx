4d9bbc08fc1c4961d5f4b0b220453fe1b1faedcd
Devagyam-199 <160000074+Devagyam-199@users.noreply.github.com>
2025-04-07 22:57:40 +0530

    main page 1.0

--- Ambulance_Booking/src/components/UserMainPage.jsx
+++ Ambulance_Booking/src/components/UserMainPage.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
-import { TileLayer, Marker, Popup, MapContainer } from "react-leaflet";
+import { TileLayer, Marker, Popup, MapContainer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import L from "leaflet";
 
+// Fix leaflet default icon paths
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetianlUrl:
+  iconRetinaUrl:
     "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
+// Custom component to recenter map when coordinates change
 const RecenterMap = ({ lat, lng }) => {
   const map = useMap();
   useEffect(() => {
@@ -23,15 +25,12 @@ const RecenterMap = ({ lat, lng }) => {
 };
 
 const UserMainPage = () => {
-  const [userLocation, setUserLocation] = useState({
-    lat: null,
-    lng: null,
-  });
+  const [userLocation, setUserLocation] = useState({ lat: null, lng: null });
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition(
-      (watchPosition) => {
-        const { latitude, longitude } = PosAnimation.coords;
+      (position) => {
+        const { latitude, longitude } = position.coords;
         setUserLocation({ lat: latitude, lng: longitude });
       },
       (error) => {
@@ -44,27 +43,27 @@ const UserMainPage = () => {
   }, []);
 
   return (
-    <>
-      <div className="h-[90vh] w-full">
-        {userLocation.lat && userLocation.lng ? (
-          <MapContainer
-            center={[userLocation.lat, userLocation.lng]}
-            zoom={16}
-            scrollWheelZoom={true}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={[userLocation.lat, userLocation.lng]} />
-            <RecenterMap lat={userLocation.lat} lng={userLocation.lng} />
-          </MapContainer>
-        ) : (
-          <p>Getting your location...</p>
-        )}
-      </div>
-    </>
+    <div className="h-[90vh] w-full">
+      {userLocation.lat && userLocation.lng ? (
+        <MapContainer
+          center={[userLocation.lat, userLocation.lng]}
+          zoom={16}
+          scrollWheelZoom={true}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[userLocation.lat, userLocation.lng]}>
+            <Popup>Your current location</Popup>
+          </Marker>
+          <RecenterMap lat={userLocation.lat} lng={userLocation.lng} />
+        </MapContainer>
+      ) : (
+        <p className="text-center text-gray-600">Getting your location...</p>
+      )}
+    </div>
   );
 };
 
