03c6217e0f19eba50fc119190e587786be901dc5
Devagyam-199 <160000074+Devagyam-199@users.noreply.github.com>
2025-04-07 23:50:46 +0530

    more ambulances

--- Ambulance_Booking/src/components/ambulanceLocation.js
+++ Ambulance_Booking/src/components/ambulanceLocation.js
@@ -2,27 +2,53 @@ export const ambulances = [
     {
       id: 1,
       name: "Ambulance 1",
-      position: [19.294233, 72.854672],
+      position: [19.294233, 72.854672], // Shanti Vidya Nagri Road
     },
     {
       id: 2,
       name: "Ambulance 2",
-      position: [19.296518, 72.850715],
+      position: [19.296518, 72.850715], // GCC Club
     },
     {
       id: 3,
       name: "Ambulance 3",
-      position: [19.292385, 72.847211],
+      position: [19.292385, 72.847211], // Samartha Hospital
     },
     {
       id: 4,
       name: "Ambulance 4",
-      position: [19.291109, 72.857821],
+      position: [19.291109, 72.857821], // Lodha Codename BullsEye
     },
     {
       id: 5,
       name: "Ambulance 5",
-      position: [19.289539, 72.851901],
+      position: [19.289539, 72.851901], // Kashigaon Road
+    },
+    // 👇 Closer to Shanti Vidya Nagri Road area
+    {
+      id: 6,
+      name: "Ambulance 6",
+      position: [19.293800, 72.855000], // 50m north of user
+    },
+    {
+      id: 7,
+      name: "Ambulance 7",
+      position: [19.293500, 72.854000], // 50m west of user
+    },
+    {
+      id: 8,
+      name: "Ambulance 8",
+      position: [19.293100, 72.855400], // 50m south-east of user
+    },
+    {
+      id: 9,
+      name: "Ambulance 9",
+      position: [19.292900, 72.853800], // 100m south-west of user
+    },
+    {
+      id: 10,
+      name: "Ambulance 10",
+      position: [19.294000, 72.854200], // Between Lodha and BullsEye
     },
   ];
   
